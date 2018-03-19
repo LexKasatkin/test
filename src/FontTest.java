e9ab2d94ded79b61b07de38f941b498acc84a65a
master <kasatkin-11@yandex.ru>
2018-03-19 15:41:59 +0700

    arraysort

--- src/FontTest.java
+++ src/FontTest.java
@@ -1,5 +1,8 @@
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
@@ -8,8 +11,8 @@ public class FontTest {
     public static void main(String [] args){
         EventQueue.invokeLater(()->
                 {
-                    JFrame jFrame=new FontFrame();
-                    jFrame.setTitle("kdfjsld");
+                    JFrame jFrame=new PlafFrame();
+                    jFrame.setTitle("PlafFrame");
                     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     jFrame.setVisible(true);
                 }
@@ -56,5 +59,118 @@ class FontComponent extends JComponent {
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(DEFAULT_WIDTH, DEFALUT_HEIGHT);
+    }
+
+
+
+}
+
+class ImageFrame extends JFrame{
+    public ImageFrame(){
+        add(new ImageComponent());
+        pack();
+    }
+}
+
+    class ImageComponent extends JComponent {
+        private static final int DEFAULT_WIDTH = 300;
+        private static final int DEFALUT_HEIGHT = 200;
+
+        private Image image;
+
+        public ImageComponent(){
+            image=new ImageIcon("jkj.png").getImage();
+        }
+        public void paintComponent(Graphics g) {
+            if (image==null)return;
+            int imageWidth=image.getWidth(this);
+            int imageHeight=image.getHeight(this);
+
+            g.drawImage(image, 0, 0, null);
+
+            for(int i=0;i*imageWidth<=getWidth();i++){
+                for (int j=0;j*imageHeight<=getHeight(); j++){
+                    if(i+j>0)
+                        g.copyArea(0, 0, imageWidth, imageHeight, i*imageWidth, j*imageHeight);
+                }
+            }
+
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(DEFAULT_WIDTH, DEFALUT_HEIGHT);
+        }
+    }
+
+    class ButtonFrame extends JFrame{
+        private JPanel buttonPanel1;
+        private static final int DEFAULT_WIDTH = 300;
+        private static final int DEFALUT_HEIGHT = 200;
+        public ButtonFrame(){
+            setSize(DEFAULT_WIDTH, DEFALUT_HEIGHT);
+            JButton yellowButton=new JButton("Yellow");
+            JButton blueButton =new JButton("Blue");
+            JButton redButton=new JButton("Red");
+
+            buttonPanel1=new JPanel();
+            buttonPanel1.add(yellowButton);
+            buttonPanel1.add(blueButton);
+            buttonPanel1.add(redButton);
+
+            add(buttonPanel1);
+
+            ColorAction yellowAction=new ColorAction(Color.YELLOW);
+            ColorAction blueAction=new ColorAction(Color.BLUE);
+            ColorAction redAction=new ColorAction(Color.RED);
+
+            yellowButton.addActionListener(yellowAction);
+            blueButton.addActionListener(blueAction);
+            redButton.addActionListener(redAction);
+
+
+        }
+
+        private class ColorAction implements ActionListener{
+            private Color backgroundColor;
+
+            public ColorAction(Color c) {
+                backgroundColor=c;
+
+            }
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                buttonPanel1.setBackground(backgroundColor);
+            }
+        }
+}
+
+class PlafFrame extends JFrame
+{
+    private JPanel buttonPanel1;
+
+    public PlafFrame() {
+        buttonPanel1=new JPanel();
+        UIManager.LookAndFeelInfo[] infos = UIManager.getInstalledLookAndFeels();
+        for (UIManager.LookAndFeelInfo info : infos)
+            makeButton(info.getName(), info.getClassName());
+
+        add(buttonPanel1);
+        pack();
+    }
+
+    private void makeButton(String name, String className){
+        JButton button=new JButton(name);
+        buttonPanel1.add(button);
+
+        button.addActionListener(event->{
+            try{
+                UIManager.setLookAndFeel(className);
+                SwingUtilities.updateComponentTreeUI(this);
+                pack();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
     }
 }
